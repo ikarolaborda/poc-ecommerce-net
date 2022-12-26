cc09947ccfe5257cd539924cc113de55f64a87e3
Ikaro C. Laborda <iclaborda@msn.com>
2022-12-26 20:22:06 +0000

    feat: add ProductsController as products CRUD

---
+++ api/Controllers/ProductsController.cs
@@ -0,0 +1,74 @@
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+             return await _context.Products.ToListAsync();
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+        
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), 201)]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+            return Created("api/products/" + product.Id, product);
+        }
+        
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Product>> UpdateProduct(int id, Product product)
+        {
+            var existingProduct = await _context.Products.FindAsync(id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            
+            existingProduct.Name = product.Name;
+            existingProduct.Description = product.Description;
+            existingProduct.Price = product.Price;
+            existingProduct.PictureUrl = product.PictureUrl ?? existingProduct.PictureUrl;
+            await _context.SaveChangesAsync();
+            return Ok(existingProduct);
+        }
+        
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var existingProduct = await _context.Products.FindAsync(id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+            _context.Products.Remove(existingProduct);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
